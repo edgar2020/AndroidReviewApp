4b13243a5a04b7b2723d3dd0c55822edcdda704e
edgar2020 <evmelendrez@gmail.com>
2019-09-17 08:56:29 -0700

    I added the randomness

--- app/src/main/java/com/example/androidreviewapp/MainActivity.java
+++ app/src/main/java/com/example/androidreviewapp/MainActivity.java
@@ -1,17 +1,45 @@
 package com.example.androidreviewapp;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static int r_value, g_value, b_value;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                r_value = (int)(Math.random()*255);
+                g_value = (int)(Math.random()*255);
+                b_value = (int)(Math.random()*255);
+                findViewById(R.id.button2).setBackgroundColor(Color.rgb(r_value, g_value, b_value));
+
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Color Changed to R: "+r_value+", G: "+g_value+", B: " +b_value,
+                        Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
 
     }
 }
